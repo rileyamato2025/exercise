dd064c1db9e1e1775cba9cd452c9ac4cb6f3f95f
Riley <ramato@eagles.bridgewater.edu>
2025-10-31 14:54:32 -0400

    new defs and errors during print. try different things in final if/else statement

--- stats.py
+++ stats.py
@@ -36,5 +36,26 @@ def prop_above(ls):
     for x in ls:
         if x > avg:
             count_above += 1
-    return count_above / len(ls)
-    
+    return count_above/len(ls)
+def median_even(ls):
+    upper_idx = int(len(ls)/2)
+    lower_idx = upper_idx - 1
+    median = (ls[upper_idx] + ls[lower_idx])/2
+
+    return median
+def median_odd(ls):
+    idx - len(ls)//2
+    median = ls[idx]
+
+    return median
+ls = read_data("exercise.csv")
+if len(read_data)%2 ==0:
+    median_even(read_data)
+else:
+    median_odd(read_data)
+print(main(read_data("exercise.csv")))
+print(average(read_data("exercise.csv")))
+print(prop_above(read_data("exercise.csv")))
+print(median_even(read_data("exercise.csv")))
+print(median_odd(read_data("exercise.csv")))
+
